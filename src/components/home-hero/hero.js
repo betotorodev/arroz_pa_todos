d838ba19dd91313922010a6a98ce10e19201cf8c
Beto Toro <beto.toro@zemoga.com>
2022-05-07 18:04:49 -0500

    [FIX]:  some layout problems with the images

--- src/components/home-hero/hero.js
+++ src/components/home-hero/hero.js
@@ -6,42 +6,45 @@ import { Arrow } from '../arrow/arrow'
 import { ContactComponent } from '../contact-component/contact-component'
 import { LargeArrow } from '../large-arrow/largeArrow'
 import { Instagram } from '../instagram/instagram'
+import Layout from '../../layout'
 
 export const HomeHero = () => {
   const {image1, image2, image3} = useGetImages()
   return (
-    <main className="hero">
-      <section className="hero-text">
-        <header className="hero-description">
-          <h1>!Hay arroz pa' todos!</h1>
-          <p>Ayúdanos a alimentar a toda la población colombiana que no tiene su comida asegurada.</p>
-          <div className="hero-callToAction">
-            <CallToAction type="blue" arrow>Donar ahora</CallToAction>
-            <ContactComponent />
-          </div>
-        </header>
-        <footer className="hero-footer">
-          <small>Síguenos en</small>
-          <LargeArrow />
-          <Instagram />
-        </footer>
-      </section>
-      <section className="hero-images">
-        <figure className="images-container">
-          <div className="background"/>
-          <img src={image1.image} alt="chefs" />
-        </figure>
-        <div className="small-images">
-          <figure className="image1">
+    <Layout>
+      <main className="hero">
+        <section className="hero-text">
+          <header className="hero-description">
+            <h1>!Hay arroz pa' todos!</h1>
+            <p>Ayúdanos a alimentar a toda la población colombiana que no tiene su comida asegurada.</p>
+            <div className="hero-callToAction">
+              <CallToAction type="blue" arrow>Donar ahora</CallToAction>
+              <ContactComponent />
+            </div>
+          </header>
+          <footer className="hero-footer">
+            <small>Síguenos en</small>
+            <LargeArrow />
+            <Instagram />
+          </footer>
+        </section>
+        <section className="hero-images">
+          <figure className="images-container">
             <div className="background"/>
-            <img src={image2.image} alt="chefs" />
+            <img src={image1.image} alt="chefs" />
           </figure>
-          <div className="image2">
-            <div className="background"/>
-            <img src={image3.image} alt="chefs" />
+          <div className="small-images">
+            <figure className="image1">
+              <div className="background"/>
+              <img src={image2.image} alt="chefs" />
+            </figure>
+            <figure className="image2">
+              <div className="background"/>
+              <img src={image3.image} alt="chefs" />
+            </figure>
           </div>
-        </div>
-      </section>
-    </main>
+        </section>
+      </main>
+    </Layout>
   )
 }
--- src/hooks/useGetImages.js
+++ src/hooks/useGetImages.js
@@ -4,7 +4,7 @@ export const useGetImages = () => {
 
   const image1 = imagesService[8];
   const image2 = imagesService[5];
-  const image3 = imagesService[10];
+  const image3 = imagesService[12];
 
 
   return {image1, image2, image3}
