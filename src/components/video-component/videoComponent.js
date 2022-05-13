b56f91acd4fc8978f6c841b7e63f8e590ec78fda
Beto Toro <beto.toro@zemoga.com>
2022-05-13 10:59:05 -0500

    [STYLE]: partners responsive

--- src/components/video-component/videoComponent.js
+++ src/components/video-component/videoComponent.js
@@ -12,7 +12,9 @@ export const VideoComponent = () => {
             <h1>Nuestro impacto social</h1>
             <p>Queremos llevar alimento a diferentes grupos, como recicladores, víctimas del conflicto armado, y otros sectores vulnerables y de escasos recursos. Si conoces alguna población que necesite nuestra ayuda, contáctanos y entregaremos #arrozpatodos.</p>
           </section>
-          <Video />
+          <div className="video-player">
+            <Video />
+          </div>
         </div>
       </Layout>
     </main>
