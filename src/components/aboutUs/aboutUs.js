4add497c8f1f7eb1871de109689ba9227281e893
Beto Toro <beto.toro@zemoga.com>
2022-05-17 10:15:16 -0500

    [STYLE]: finish about us

---
+++ src/components/aboutUs/aboutUs.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from '../../layout'
+import './aboutUs.css'
+
+export const AboutUs = (props) => {
+  return (
+    <main className="aboutUS">
+      <Layout>
+        <h1>Sobre nosotros</h1>
+        <p>“Arroz Pa’ Todos nace como una iniciativa para ayudar a familias que no tienen asegurada su alimentación. Decidimos usar los esfuerzos y recursos de la industria gastronómica para brindar alimento a quienes más lo necesitan. Usamos las cocinas de todos los restaurantes aliados para preparar almuerzos a base de arroz. Gracias a las donaciones, a nuestros proveedores y restaurantes aliados y al apoyo de otras entidades, distribuimos alimento a las poblaciones más vulnerables. Queremos dibujar más de 5.000 sonrisas en la segunda etapa y juntos podemos lograrlo”</p>
+      </Layout>
+    </main>
+  )
+}
--- src/pages/About/about.js
+++ src/pages/About/about.js
@@ -2,12 +2,14 @@ import './about.css'
 import { Header } from '../../components/header/Header'
 import { Footer } from '../../components/footer/footer'
 import { AboutHero } from '../../components/about-hero/hero'
+import { AboutUs } from '../../components/aboutUs/aboutUs'
 
 export default function About() {
   return (
     <>
       <Header />
       <AboutHero />
+      <AboutUs />
       <Footer />
     </>
   )
