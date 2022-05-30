2d8030d334a965ea6aa8ad0473f8ce7b86551870
Beto Toro <beto.toro@zemoga.com>
2022-05-30 17:28:41 -0500

    [FEAT]: finish logos in partners page

--- src/pages/Partners/Partners.js
+++ src/pages/Partners/Partners.js
@@ -2,12 +2,14 @@ import './Partner.css'
 import { Header } from '../../components/header/Header'
 import { Footer } from '../../components/footer/footer'
 import { PartnersHero } from '../../components/partners-hero/partnersHero'
+import { PartnersLogos } from '../../components/partnersLogos/partnersLogos'
 
 export default function Partners() {
   return (
     <>
       <Header />
       <PartnersHero />
+      <PartnersLogos />
       <Footer />
     </>
   )
