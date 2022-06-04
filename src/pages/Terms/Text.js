f93818aac87ff9a2a435a6dec3b2163d9f445d23
Beto Toro <beto.toro@zemoga.com>
2022-06-04 13:07:59 -0500

    [FEAT]: finish terms and conditions

---
+++ src/pages/Terms/Text.js
@@ -0,0 +1,139 @@
+import React from 'react'
+
+export const Text = () => {
+  return (
+    <>
+      <h1>Introducción</h1>
+      <br />
+      <br />
+      <p>El presente documento describe los términos y condiciones aplicables a todos los procesos que se lleven a
+        cabo por medio de la iniciativa Arroz Pa’ Todos.</p>
+      <br />
+      <p>Todos los usuarios de la página deberán leer y aceptar estos los Términos y Condiciones para usar todos los
+        servicios e información que se facilitan desde el Sitio Web. El mero acceso y/o utilización del Sitio Web, de
+        todos o parte de sus contenidos y/o servicios significa la plena aceptación de las presentes condiciones
+        generales de uso. Los usuarios no podrán tomar medidas particulares o legales que desconozcan lo descrito en
+        esta sección.</p>
+      <br />
+      <p>La iniciativa Arroz Pa’ Todos se reserva la facultad de modificar en cualquier momento las condiciones
+        generales de uso del Sitio Web. En todo caso, se recomienda que consulte periódicamente los presentes términos,
+        ya que pueden ser modificados.</p>
+      <br />
+      <br />
+      <h3>2. Condiciones de uso</h3>
+      <br />
+      <h5>2.1. Responsabilidad del usuario:</h5>
+      <br />
+      <p>El usuario deberá respetar en todo momento los términos y condiciones  establecidos en las presentes
+        condiciones generales de uso del Sitio Web. De forma expresa el usuario manifiesta que utilizará el portal de
+        forma diligente y asumiendo cualquier responsabilidad que pudiera derivarse de su uso.</p>
+      <br />
+      <h5>2.2. Responsabilidad de Arroz Pa’ Todos:</h5>
+      <br />
+      <p>Los promotores de la iniciativa Arroz Pa’ Todos, restaurantes, proveedores, distribuidores y entidades
+        beneficiadas se comprometen a no usar el Sitio Web con fines fraudulentos, de estafa, hurto, daño temporal o
+        permanente de imagen, intereses, propósitos y derechos de las partes involucradas, de terceros o de la
+        iniciativa. Las partes se comprometen a actuar bajo el principio de buena fe establecido en el artículo 83 de la
+        Constitución Política de la República de Colombia.</p>
+      <br />
+      <p>Exceptuando los casos en los que la Ley imponga expresamente lo contrario, y exclusivamente con la medida y
+        extensión en que lo imponga, la iniciativa Arroz Pa’ Todos no garantiza ni asume responsabilidad alguna respecto
+        a los posibles daños y perjuicios causados por el uso de la información, datos y servicios del Sitio Web.
+      </p>
+      <br />
+      <p>En todo caso, la iniciativa Arroz Pa’ Todos excluye cualquier responsabilidad por los daños y perjuicios
+        que puedan deberse a la información y/o servicios prestados o suministrados por terceros diferentes de la
+        Empresa. Toda responsabilidad será del tercero ya sea proveedor o colaborador.</p>
+      <br />
+      <h5>2.3. Tratamiento de datos y propiedad intelectual</h5>
+      <br />
+      <p>Toda la información suministrada por medio del formulario de contacto estará protegida de conformidad con
+        la Política de Tratamiento de Datos, contemplada en la Ley de Habeas Data (1581 de 2012).</p>
+      <br />
+      <p>Todos los contenidos, marcas, logos, fotografías, datos o cualquier otro elemento susceptible de protección
+        por la legislación de propiedad intelectual o industrial, que sean accesibles en el Sitio Web corresponden
+        exclusivamente a la iniciativa Arroz Pa’ Todos y a los restaurantes, proveedores, distribuidores y entidades
+        beneficiadas. Queda expresamente prohibido el uso de cualquiera de dichos elementos sin la autorización previa
+        de las partes involucradas.</p>
+      <br />
+      <p>En cualquier caso, la iniciativa Arroz Pa’ Todos se reserva todos los derechos sobre los contenidos,
+        información y datos que ostente sobre los mismos.</p>
+      <br />
+      <p>Las partes involucradas en la iniciativa Arroz Pa’ Todos deberán mantener la confidencialidad de la
+        información suministrada por usuarios o aliados y no podrán divulgarla, en todo o en parte, a persona alguna sin
+        previa autorización.
+        En caso de autorizar la
+        divulgación a terceros, deberán acogerse al presente acuerdo de confidencialidad.</p>
+      <br />
+      <h5>2.4. Incumplimiento de Términos y Condiciones</h5>
+      <br />
+      <p>En caso de incumplimiento de los Términos y Condiciones por parte de usuarios del Sitio Web, la iniciativa
+        Arroz Pa’ Todos se reserva el derecho a limitar, suspender, bloquear o terminar su acceso al Sitio Web,
+        adoptando las medidas técnicas necesarias para este fin.</p>
+      <br />
+      <br />
+      <h3>3. Donaciones</h3>
+      <br />
+      <p>Las donaciones de particulares, empresas o cualquier interesado se realizarán por medio de la plataforma
+        Arma Tu Vaca, siguiendo los siguientes lineamientos:</p>
+      <br />
+      <p>El fondo a recaudar será de $35’000.000 para la tercera etapa de la iniciativa.</p>
+      <br />
+      <p>Cada parte interesada decidirá libremente el monto que desee donar.</p>
+      <br />
+      <p>La recaudación se realiza por medio de la plataforma Arma Tu Vaca. La iniciativa Arroz Pa’ Todos se exime
+        de toda responsabilidad ante cualquier imprevisto en el proceso de pago y ante el suministro de información
+        errónea, falsa o inexacta que se derive de dicho proceso.</p>
+      <br />
+      <p>La plataforma cobrará el 2.99% +IVA de comisión del valor total solicitado.</p>
+      <br />
+      <p>El dinero recaudado se destinará exclusivamente a la compra de alimentos y empaques con el apoyo y
+        descuento de los proveedores aliados.</p>
+      <br />
+      <p>Los movimientos, soportes de compra y recaudo podrán ser solicitados por particulares, empresas o cualquier
+        parte interesada.</p>
+      <br />
+      <p>El dinero será gestionado por los organizadores de Arroz Pa’ Todos y los colaboradores de la Fundación
+        Jardín de Amor.</p>
+      <br />
+      <p>Los recursos con los cuales se efectúa el pago de las donaciones deben provenir de conductas consagradas en
+        la legislación colombiana, evitando cualquier conducta consagrada como falta constitutiva de infracción
+        penal.</p>
+      <br />
+      <br />
+      <h3>4. Restaurantes</h3>
+      <br />
+      <p>Los restaurantes interesados deberán diligenciar el formulario de contacto con datos válidos y reales para
+        solicitar más información acerca de la iniciativa. Cada restaurante garantiza la veracidad, exactitud,
+        autenticidad y vigencia de los datos suministrados.</p>
+      <br />
+      <p>Todos los restaurantes deben cumplir con las siguientes condiciones para su participación en la iniciativa
+        Arroz Pa’ Todos:</p>
+      <br />
+      <p>Contar con una capacidad de producción mínima de 100 platos de arroz en una sola entrega.</p>
+      <br />
+      <p>Aplicar los protocolos de higiene y sanidad necesarios y adecuados a las circunstancias actuales.
+      </p>
+      <br />
+      <p>Informar oportunamente acerca de dichos protocolos a los encargados de la iniciativa Arroz Pa’
+        Todos.</p>
+      <br />
+      <p>Arroz Pa’ Todos se compromete a hacer uso responsable del nombre y la imagen de cada uno de los
+        restaurantes aliados bajo previa autorización.</p>
+      <br />
+      <p>La iniciativa
+        se reserva el derecho a rechazar alguna solicitud o participación por incumplimiento de Términos y Condiciones o
+        por circunstancias que puedan ocurrir en el proceso. Arroz Pa’ Todos se compromete a informar oportunamente
+        sobre cualquier decisión acerca de la participación de un restaurante aliado, exponiendo las causas para su
+        admisión o rechazo.</p>
+      <br />
+      <p>Cada restaurante debe comprometerse a brindar cualquier información solicitada por donantes o terceros, con el fin de velar por la transparencia de
+        cada proceso.</p>
+      <br />
+      <br />
+      <h3>5. Consideraciones finales</h3>
+      <br />
+      <p>Cualquier inquietud, comentario o controversia que surja respecto a los establecido en el presente documento podrá ser resuelto mediante contacto entre las partes.</p>
+    </>
+  )
+}
